d0d3313919b3a9cbaaea0041b9b0d82c5a6c6095
byeollim woo <byeollim.woo@gmail.com>
2023-01-27 18:10:22 +0900

    commit

---
+++ src/com/greedy/section01/config/Application.java
@@ -0,0 +1,11 @@
+package com.greedy.section01.config;
+
+import static com.greedy.common.Template.getSqlSession;
+
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println(getSqlSession());
+	}
+
+}
